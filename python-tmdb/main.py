428cd7025f8b47c9b16903da5a16c2625b195238
Gustav Rasmussen <TheNewThinkTank@users.noreply.github.com>
2025-07-13 22:00:07 +0200

    chore: list movies by actor, sorted by date

--- python-tmdb/main.py
+++ python-tmdb/main.py
@@ -1,5 +1,6 @@
 
 import os
+from pprint import pprint as pp
 from dotenv import load_dotenv
 import tmdbsimple as tmdb
 
@@ -14,11 +15,38 @@ def main():
     # response = movie.info()
     # print(movie.title)
 
-    search = tmdb.Search()
+    # search = tmdb.Search()
     # response = search.movie(query='The Bourne')
-    response = search.movie(query='Jackie Chan')
-    for s in search.results:
-        print(s['title'], s['id'], s['release_date'], s['popularity'])
+    # for s in search.results:
+    #     print(s['title'], s['id'], s['release_date'], s['popularity'])
+
+    # search.person(query='Jackie Chan')
+    # for s in search.results:
+    #     print(s['name'], s['id'], s['popularity'])  # 18897
+
+    person = tmdb.People(18897)
+    person.info()
+    # print(person.name)
+    # print(person.biography)
+    # print(person.place_of_birth)
+    # print(person.birthday)
+    # print(person.deathday)
+    # print(person.also_known_as)
+    # print(person.changes())
+
+    # print(type(person.movie_credits()))  # <class 'dict'>
+    # print(person.movie_credits().keys())  # dict_keys(['cast', 'crew', 'id'])
+    # pp(person.movie_credits()['cast'])
+    # pp(person.movie_credits()['crew'])
+    # pp(person.movie_credits())
+
+    movies = [
+        (movie['title'], movie['release_date'], movie['popularity'])
+        for movie in person.movie_credits()['cast']   
+        if movie['release_date']
+    ]
+    movies.sort(key=lambda x: x[1], reverse=True)  # Sort by release date
+    pp(movies)
 
 
 if __name__ == "__main__":
